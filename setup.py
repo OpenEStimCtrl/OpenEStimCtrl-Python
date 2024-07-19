e72c1761bc8e2d7868b78e5b4bbe8560e1667042
Sound Reload <wmliyin@outlook.com>
2024-07-20 02:38:11 +0800

    feat: 添加符合包的文件

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DESCRIPTION = 'Python binding for OpenEStimCtrl'
 URL = 'https://github.com/OpenEStimCtrl/OpenEStimCtrl-Python'
 AUTHOR = 'Sound Reload'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -102,7 +102,7 @@ setup(
     author=AUTHOR,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"], include=['openestimctrl', 'openestimctrl.*']),
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['openestimctrl'],
 
